a02037f44b592227d70bcd171814c022323e9e4e
hubbu <ia@ivanarellano.com>
2012-03-28 17:02:37 -0700

    Hello assets

--- pm-desktop/src/com/ivanarellano/game/pm/pmDesktop.java
+++ pm-desktop/src/com/ivanarellano/game/pm/pmDesktop.java
@@ -1,9 +1,20 @@
 package com.ivanarellano.game.pm;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker.Settings;
 
 public class pmDesktop {
 	public static void main(String args[]) {
+        Settings settings = new Settings();
+        settings.padding = 6;
+        settings.edgePadding = false;
+        settings.maxWidth = 1024;
+        settings.maxHeight = 1024;
+        settings.incremental = true;
+        settings.pot = true;
+        TexturePacker.process(settings, "assets", "data");
+        
 		new LwjglApplication(new pmGame(), "Pestalotiopsis Microspora",
 				pmGame.SCREEN_WIDTH, pmGame.SCREEN_HEIGHT, false);
 	}
