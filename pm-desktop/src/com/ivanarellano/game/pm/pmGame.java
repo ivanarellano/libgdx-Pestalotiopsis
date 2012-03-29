102df0f2a8c3817d05d31635316ac603e42879d4
hubbu <ia@ivanarellano.com>
2012-03-28 17:49:38 -0700

    Init screen creation

--- pm-desktop/src/com/ivanarellano/game/pm/pmGame.java
+++ pm-desktop/src/com/ivanarellano/game/pm/pmGame.java
@@ -2,14 +2,21 @@ package com.ivanarellano.game.pm;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class pmGame extends Game implements ApplicationListener {
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
+	
+	public static Stage stage = new Stage(SCREEN_WIDTH, SCREEN_HEIGHT, false);
+	public ScreenStack screenStack = new ScreenStack(this);
 
 	@Override
 	public void create() {
-
+		Gdx.app.log("libGdx ver", Integer.toString(Gdx.app.getVersion()));
+		
+		Gdx.graphics.setVSync(true);
 	}
 
 	@Override
---
+++ pm-desktop/src/com/ivanarellano/game/pm/screen/Game.java
@@ -0,0 +1,35 @@
+package com.ivanarellano.game.pm.screen;
+
+import com.badlogic.gdx.Screen;
+
+public class Game implements Screen {
+
+	@Override
+	public void render(float delta) {		
+	}
+
+	@Override
+	public void resize(int width, int height) {		
+	}
+
+	@Override
+	public void show() {		
+	}
+
+	@Override
+	public void hide() {		
+	}
+
+	@Override
+	public void pause() {		
+	}
+
+	@Override
+	public void resume() {		
+	}
+
+	@Override
+	public void dispose() {		
+	}
+
+}
