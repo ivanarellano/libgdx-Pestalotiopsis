36c84f9065fb7ee3899aae21d3399071aefdd517
hubbu <ia@ivanarellano.com>
2012-03-30 15:19:29 -0700

    Apply pretty style to Tile. Make it a Group.

--- pm-desktop/src/com/ivanarellano/game/pm/Tile.java
+++ pm-desktop/src/com/ivanarellano/game/pm/Tile.java
@@ -1,25 +1,24 @@
 package com.ivanarellano.game.pm;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.ivanarellano.game.pm.Assets.LabelStyles;
 
-public class Tile extends Actor {
-	TextureRegion region = Assets.atlas.findRegion("tile");
+public class Tile extends Group {
+	Image texture = new Image(Assets.atlas.findRegion("tile"));
 	public String number = new String();
+	Label label;
 	
 	public Tile(String number) {
 		this.number = number;
-	}
-	
-	@Override
-	public void draw(SpriteBatch batch, float parentAlpha) {
-		batch.draw(region, x, y);
-	}
-
-	@Override
-	public Actor hit(float x, float y) {
-		return null;
+		label = new Label(number, LabelStyles.TILE_NUMBERS);
+		
+		label.x = texture.width/2 - label.getTextBounds().width/2;
+		label.y = -35;
+		
+		addActor(texture);
+		addActor(label);
 	}
 
 }
