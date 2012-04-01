bc99304657794bc7d04e551325dbef10d8113eb3
hubbu <ia@ivanarellano.com>
2012-04-01 03:25:59 -0700

    Activate GameOverUI elements

--- pm-desktop/src/com/ivanarellano/game/pm/GameOverUI.java
+++ pm-desktop/src/com/ivanarellano/game/pm/GameOverUI.java
@@ -4,24 +4,41 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.ivanarellano.game.pm.Assets.LabelStyles;
+import com.ivanarellano.game.pm.screen.GameScreen;
 
-public class GameOverUI extends Group {
-	PmGame game;
+public class GameOverUI extends Group {	
+	GameScreen gameScreen;
 	
+	Image newGame;
+	Image quitGame;
 	Image tintOverlay = new Image(Assets.atlas.findRegion("tintoverlay"));
 	Image logo = new Image(Assets.atlas.findRegion("logogameover")); 
-	Image newGame = new Image(Assets.atlas.findRegion("newlandgameover"));
-	Image quitGame = new Image(Assets.atlas.findRegion("quitgameover"));
 	Image scoreText = new Image(Assets.atlas.findRegion("scoregameover"));
 	Label scoreNumber = new Label("0", LabelStyles.MOVES_COUNTER);
-	
-	boolean isActive = false;
-	
-	public GameOverUI(PmGame game) {
-		this.game = game;
+		
+	public GameOverUI(final GameScreen game) {
+		gameScreen = game;
 		
 		logo.x = PmGame.SCREEN_WIDTH - logo.width - 10;
 		logo.y = PmGame.SCREEN_HEIGHT - logo.height - 40;
+		
+		newGame = new Image(Assets.atlas.findRegion("newlandgameover")) {
+			@Override
+			public boolean touchDown(float x, float y, int pointer) {
+				gameScreen.resetBoard();
+				removeUI();
+				gameScreen.state = GameState.READY;
+				return super.touchDown(x, y, pointer);
+			}
+		};
+		
+		quitGame = new Image(Assets.atlas.findRegion("quitgameover")) {
+			@Override
+			public boolean touchDown(float x, float y, int pointer) {
+				gameScreen.game.screenStack.setPrevious();
+				return super.touchDown(x, y, pointer);
+			}
+		};
 		
 		newGame.touchable = true;
 		newGame.x = PmGame.SCREEN_WIDTH - newGame.width - 10;
@@ -37,26 +54,23 @@ public class GameOverUI extends Group {
 		scoreText.x = PmGame.SCREEN_WIDTH - scoreText.width - 156;
 		scoreText.y = 20;
 		
-		tintOverlay.setFillParent(true);
+		tintOverlay.scaleX = 3.0f;
+		tintOverlay.scaleY = 3.0f;
 	}
 	
-	public void init() {
-		isActive = true;
-		
+	public void init() {		
+		addActor(tintOverlay);
 		addActor(logo);
 		addActor(newGame);
 		addActor(quitGame);
 		addActor(scoreNumber);
 		addActor(scoreText);
 		
-		game.stage.addActor(tintOverlay);
-		game.stage.addActor(this);
+		gameScreen.game.stage.addActor(this);
 	}
 	
-	public void removeUI() {
-		isActive = false;
-		
-		game.stage.removeActor(tintOverlay);
-		game.stage.removeActor(this);
+	public void removeUI() {		
+		gameScreen.game.stage.removeActor(this);
 	}
+	
 }
