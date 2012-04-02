8b5b191daf420575a15b569d6fe65044b14953aa
hubbu <ia@ivanarellano.com>
2012-04-01 18:10:22 -0700

    Finish applet setup

--- pm-desktop/src/com/ivanarellano/game/pm/PmApplet.java
+++ pm-desktop/src/com/ivanarellano/game/pm/PmApplet.java
@@ -1,5 +1,10 @@
 package com.ivanarellano.game.pm;
 
-public class PmApplet {
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
 
+public class PmApplet extends LwjglApplet {
+    private static final long serialVersionUID = 1L;
+    public PmApplet() {
+    	super (new PmGame(), false);
+    }
 }
